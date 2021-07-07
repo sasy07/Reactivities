e754ce9e7915e1592f5883ec2e072574448df408
Sasy07 <sasan.ghafooryfard@gmail.com>
2021-07-08 03:34:14 +0430

    First Commit S02

---
+++ API/Program.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+            var service = scope.ServiceProvider;
+            try
+            {
+                var context = service.GetRequiredService<DataContext>();
+                context.Database.Migrate();
+                await Seed.SeedData(context);
+            }
+            catch (Exception ex)
+            {
+                var logger = service.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "Migrate failed");
+            }
+
+            await host.RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
